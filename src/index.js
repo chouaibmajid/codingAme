e0248fbeea59bf60c40888e2cdb083e94acb7743
Majid chouaib <majidchouaib5@gmail.com>
2021-10-05 23:36:44 +0200

    ajout axios main 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import axios from "axios"
 ReactDOM.render(
   <React.StrictMode>
     <App />
