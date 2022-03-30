91b51d33556af522d2bf54e950fbadda373b487d
chouaibmajid <majidchouaib5@gmail.com>
2022-03-30 20:57:45 +0200

    add function js

--- src/WelcomeTitle.js
+++ src/WelcomeTitle.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+// Create the WelcomeTitle React component​​​​​​‌​​‌‌​​​‌‌‌​‌‌‌​​‌‌​‌‌‌‌​ here
+
+// Modify this function if you want to change the preview
+// It will not be evaluated as part of the assessment
 
 function WelcomeTitle({user , primary}) {
     return (
-        <div>
-          {primary ? <h1>`Welcome ${user}!</h1> : <h2>Welcome!</h2>}  
-        </div>
+        <h2>
+          {primary ? <h1>Welcome {user}!</h1> : <h2>Welcome!</h2>}  
+        </h2>
     )
 }
 
-export default WelcomeTitle
+// Do not change
+export default WelcomeTitle;
