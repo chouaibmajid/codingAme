9fd84fb2bdf69a6e6445bfd1889b079fbd260754
chouaibmajid <majidchouaib5@gmail.com>
2022-04-25 14:20:02 +0100

    add objet

--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import React from "react";
-import { createRef } from "react";
-
+import { samuel } from "./objet";
 function App() {
-  const [search, setSearch] = React.useState("");
-  const [data, setData] = React.useState([]);
-  const testDivRef = React.useRef(null);
-  const [elRefs, setElRefs] = React.useState([]);
-
-  React.useEffect(() => {
-    setElRefs((elRefs) =>
-      Array(data.length)
-        .fill()
-        .map((_, i) => elRefs[i] || createRef())
-    );
-  }, [data]);
-  console.log(elRefs);
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setData([...data, search]);
-          testDivRef.current.scrollIntoView({
-            behavior: "smooth",
-          });
-        }}
-      >
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button>envoyer</button>
-      </form>
-      <div style={{ overflow: "scroll", height: "100px" }}>
-        {!!data.length &&
-          data.map((item, id) => (
-            <div
-            ref={elRefs[id]}
-              key={id}
-              style={{ color: "red" }}
-              onClick={() => elRefs[id].current.style.color= "green" }
-            >
-              {item}
-            </div>
-          ))}
-        <div ref={testDivRef}></div>
+  const [data,setData] = React.useState()
+  const convertToTable = () => {
+    var newArr = [];
+    for(let key in samuel){
+      newArr.push({
+      id:key,
+      ...samuel[key],
+      })
+    }
+    setData(newArr)
+  }
+ React.useEffect(() => {
+    convertToTable();
+  }, []);
+  console.log(data)
+  if(!data) return <p>...Loading</p>
+  return (<div>
+    
+    {data.map(item => (
+      <div>
+        <p>{item.name}</p>
+        <p>{item.lastName}</p>
       </div>
-    </>
-  );
+    ))}
+    
+    </div>);
 }
 
 export default App;
