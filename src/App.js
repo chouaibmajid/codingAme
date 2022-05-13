033643c867a62a2da38fc3075ad0099425d5886d
MAJID Chouaib <chouaib.majid@akka.eu>
2022-05-13 23:16:11 +0200

    add extensions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import POints3d from './POints3d'
+import React from "react";
+import POints3d from "./POints3d";
 
 function App() {
-  const[showGraph,setShowGraph]=React.useState(false)
+  const [showGraph, setShowGraph] = React.useState(false);
 
   return (
     <>
-    <div style={{display: "flex",justifyContent:'center',marginTop:"15px"}}>
-      <button
-      onClick={() => setShowGraph(true)}
-      style={{color: "white", backgroundColor: "green",padding: "10px",fontSize:"15px"}}
-      >show results</button>
-    </div>
-    {showGraph && <div><POints3d /></div>}
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "15px" }}
+      >
+        <button
+          onClick={() => setShowGraph(true)}
+          style={{
+            color: "white",
+            backgroundColor: "green",
+            padding: "10px",
+            fontSize: "15px",
+          }}
+        >
+          show results
+        </button>
+      </div>
+      {showGraph && (
+        <div>
+          <POints3d />
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
