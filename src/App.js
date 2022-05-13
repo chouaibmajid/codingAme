fd52344e8f5de917ee1f3d7dad0607225faf9c49
MAJID Chouaib <chouaib.majid@akka.eu>
2022-05-13 23:03:37 +0200

    add show button 3d

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React from 'react'
 import POints3d from './POints3d'
 
 function App() {
+  const[showGraph,setShowGraph]=React.useState(false)
+
   return (
-    <div><POints3d /></div>
+    <>
+    <div style={{display: "flex",justifyContent:'center',marginTop:"15px"}}>
+      <button
+      onClick={() => setShowGraph(true)}
+      style={{color: "white", backgroundColor: "green",padding: "10px",fontSize:"15px"}}
+      >show results</button>
+    </div>
+    {showGraph && <div><POints3d /></div>}
+    </>
   )
 }
 
